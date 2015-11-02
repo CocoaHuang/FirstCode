2fb3ed56b7a330965f5a4d621c8f803eba0201df
FIRST_NAME LAST_NAME <codehuang@163.com>
2015-11-02 14:16:21 +0800

    提交

--- test/test/AppDelegate.m
+++ test/test/AppDelegate.m
@@ -18,7 +18,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.backgroundColor = [UIColor whiteColor];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = vc;
     [self.window makeKeyWindow];
     
     return YES;
