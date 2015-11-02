8b4712ea7824a707b72cce9eec584a90befb7e66
FIRST_NAME LAST_NAME <codehuang@163.com>
2015-11-02 14:12:02 +0800

    Revert "刚刚修改的"
    This reverts commit 1e92b56b352089f6b15a0c2b5dfb232da0d40266.

--- test/test/AppDelegate.m
+++ test/test/AppDelegate.m
@@ -18,12 +18,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyWindow];
     
-    UIViewController *vc = [[UIViewController alloc] init];
-    vc.view.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = vc;
-    
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
